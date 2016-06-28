6e440130b837b3ed8fd8985d2153fd35dd5a1bdb
Garrett Biernat and Reed Lambier <Guest@epicodus-13.local>
2016-06-28 16:56:45 -0700

    fixed focus bug

--- app/src/main/java/net/gdbmedia/boggle/MainActivity.java
+++ app/src/main/java/net/gdbmedia/boggle/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
