4e6f79cd01acfd12a455b777c0f405e2960d3ae8
Garrett Biernat and Reed Lambier <Guest@epicodus-13.local>
2016-06-28 14:07:02 -0700

    finished logic

--- app/src/main/java/net/gdbmedia/boggle/AnswerList.java
+++ app/src/main/java/net/gdbmedia/boggle/AnswerList.java
@@ -1,13 +1,46 @@
 package net.gdbmedia.boggle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class AnswerList extends AppCompatActivity {
+    @Bind(R.id.listView) ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer_list);
+        Intent intent = getIntent();
+        ButterKnife.bind(this);
+
+
+        ArrayList answersArray = intent.getStringArrayListExtra("answers");
+        Log.d("this is my array", "arr: " + answersArray.get(0).toString());
+
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, answersArray);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String answer = ((TextView)view).getText().toString();
+                Toast.makeText(AnswerList.this, answer, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/net/gdbmedia/boggle/MainActivity.java
+++ app/src/main/java/net/gdbmedia/boggle/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.letters) TextView mLetters;
     @Bind(R.id.answer) EditText mAnswer;
     @Bind(R.id.submitButton) Button mSubmit;
+    private ArrayList mAnswers = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,16 +45,13 @@ public class MainActivity extends AppCompatActivity {
                 String answer = mAnswer.getText().toString();
                 char[] answerArray = answer.toUpperCase().toCharArray();
                 if(answerArray.length >=3){
-                    String b = new String(lettersArray);
-                    Log.d("char Array", b);
                     for(int i = 0; i<answerArray.length; i++){
-                        String test = "" + answerArray[i];
-                        Log.d("letter " + i, test);
                         if(new String(lettersArray).indexOf(answerArray[i]) != -1){
                             Toast.makeText(MainActivity.this, "we made it here", Toast.LENGTH_LONG).show();
                             if(i == answerArray.length-1){
+                                mAnswers.add(answer);
                                 Intent intent = new Intent(MainActivity.this, AnswerList.class);
-                                intent.putExtra("answer", answer);
+                                intent.putExtra("answers", mAnswers);
                                 startActivity(intent);
                             }
                         }else{
@@ -60,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
                             break;
                         }
                     }
+                }else {
+                    Toast.makeText(MainActivity.this, "Length of Answer is less than 3", Toast.LENGTH_LONG).show();
                 }
 
             }
@@ -71,18 +72,18 @@ public class MainActivity extends AppCompatActivity {
         String[] others = { "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "X", "Z", "W", "Y", "A", "E", "I", "O", "U"};
         String returnValue = "";
         Random rand = new Random();
-        int vowel1 = rand.nextInt((7 - 0) + 1) + 0;
-        int vowel2 = rand.nextInt((7 - 0) + 1) + 0;
+        int vowel1 = rand.nextInt((7 - 0) + 1);
+        int vowel2 = rand.nextInt((7 - 0) + 1);
         while (vowel1 == vowel2){
-            vowel2 = rand.nextInt((7 - 0) + 1) + 0;
+            vowel2 = rand.nextInt((7 - 0) + 1);
         }
         for(int i = 0; i<8; i++){
 
             if(i == vowel1 || i == vowel2) {
-                int x = rand.nextInt((4 - 0) + 1) + 0;
+                int x = rand.nextInt((4 - 0) + 1);
                 returnValue += vowels[x];
             }else{
-                int x = rand.nextInt((25 - 0) + 1) + 0;
+                int x = rand.nextInt((25 - 0) + 1);
                 returnValue += others[x];
             }
         }
